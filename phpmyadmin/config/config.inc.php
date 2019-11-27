e98d191f55a36a11f1812de98a151c61fb59e877
Bastiaan de Hart <8478702+Bastiaantjuhh@users.noreply.github.com>
2019-11-27 22:47:25 +0100

    Repo init

---
+++ phpmyadmin/config/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+    /* Authentication type */
+    $cfg['Servers'][$i]['auth_type'] = 'config';
+    /* Server parameters */
+    $cfg['Servers'][$i]['host'] = 'mysql';
+    $cfg['Servers'][$i]['user'] = 'root';
+    $cfg['Servers'][$i]['password'] = 'root';
+    $cfg['Servers'][$i]['extension'] = 'mysqli';
+    $cfg['Servers'][$i]['compress'] = false;
+    $cfg['Servers'][$i]['AllowNoPassword'] = false;
+?>
